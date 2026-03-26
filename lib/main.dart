00a6842af7fa951e06738a113c8d75f5579256d7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 19:50:40 -0300

    Desenvolvimento inicial do app IronKey para gerar senhas

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ironkey/app_theme.dart';
+
+void main() {
+  runApp(IronKeyApp());
+}
+
+class IronKeyApp extends StatelessWidget {
+  const IronKeyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      title: "IronKey",
+      home: ,
+    );
+  }
+}
