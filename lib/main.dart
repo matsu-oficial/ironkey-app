f3a96969ab096e0854643f5c00691d531b55e12c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 21:04:35 -0300

    add: logica do gerador de senhas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ironkey/app_theme.dart';
 
 void main() {
@@ -16,7 +19,129 @@ class IronKeyApp extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
       title: "IronKey",
-      home: ,
+      home: IronKeyScreen(),
+    );
+  }
+}
+
+class IronKeyScreen extends StatefulWidget {
+  const IronKeyScreen({super.key});
+
+  @override
+  State<IronKeyScreen> createState() => _IronKeyScreenState();
+}
+
+class _IronKeyScreenState extends State<IronKeyScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void copyPassword(String password) {
+    Clipboard.setData(ClipboardData(text: password));
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Senha copiada!')));
+  }
+
+  void generatePassword() {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#\$%&*";
+
+    final chars = upper + lower + numbers + symbols;
+    final random = Random();
+    setState(() {
+      _passwordController.text = List.generate(
+        12,
+        (_) => chars[random.nextInt(chars.length)],
+      ).join();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 16.0,
+            horizontal: 100.0,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    ClipOval(
+                      child: Container(
+                        color: Colors.red,
+                        child: SizedBox(
+                          width: 150,
+                          height: 150,
+                          child: Image.asset(
+                            "assets/images/ironman-logo.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "Sua senha como você nunca viu!!!",
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      controller: _passwordController,
+                      maxLength: 12,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                        prefix: Icon(Icons.lock),
+                        suffix: _passwordController.text.isNotEmpty
+                            ? IconButton(
+                                onPressed: () {
+                                  copyPassword(_passwordController.text);
+                                },
+                                icon: Icon(Icons.copy),
+                              )
+                            : null,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: generatePassword,
+                  child: Text("Gerar senha"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
